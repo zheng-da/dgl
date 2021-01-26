a48070e3b08523ad2cb321c09e05edfe802ab550
Da Zheng <zhengda1936@gmail.com>
2021-01-26 02:20:18 +0000

    new method to sample neighbors.

--- src/graph/sampling/neighbor/neighbor.cc
+++ src/graph/sampling/neighbor/neighbor.cc
@@ -232,5 +232,161 @@ DGL_REGISTER_GLOBAL("sampling.neighbor._CAPI_DGLSampleNeighborsTopk")
     *rv = HeteroGraphRef(subg);
   });
 
+template<class T>
+std::vector<T> &get_vector() {
+  static thread_local std::vector<T> vec;
+  return vec;
+}
+
+template<class T>
+class hashtable
+{
+  std::vector<T> &hmap;
+  T mask;
+ public:
+  hashtable(long n): hmap(get_vector<T>()) {
+    long new_size;
+    for (new_size=1; new_size<3*n; new_size*=2);
+    mask = new_size-1;
+    if (new_size > (long) hmap.size()) {
+      hmap.resize(new_size, -1);
+    } else {
+      for (long i = 0; i < new_size; i++)
+        hmap[i] = -1;
+    }
+  }
+
+  ~hashtable() {
+  }
+
+  bool insert(T k) {
+    long j;
+    for (j=(k&mask); hmap[j]!=-1 && hmap[j]!=k; j=((j+1)&mask));
+    if (hmap[j] == -1) {
+      hmap[j] = k;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool contain(T k) const {
+    long j;
+    for (j=(k&mask); hmap[j]!=-1 && hmap[j]!=k; j=((j+1)&mask));
+    return hmap[j] != -1;
+  }
+};
+
+template<class T>
+class BufferData {
+  hashtable<T> map;
+  std::vector<T> arr;
+ public:
+  BufferData(const T*raw_arr, int64_t len): map(len) {
+    for (int64_t i = 0; i < len; i++) {
+      map.insert(raw_arr[i]);
+      arr.push_back(raw_arr[i]);
+      std::sort(arr.begin(), arr.end());
+    }
+  }
+
+  std::pair<std::vector<T>, std::vector<int64_t>> lookup(const T*raw_arr, int64_t len) const {
+    std::vector<T> res;
+    std::vector<int64_t> offs;
+    // If this input vector is large, we can scan the input vector with the
+    // sorted buffer to check the overlap.
+    if (len >= res.size() / 2) {
+      res.resize(std::max((size_t) len, res.size()));
+      auto it = std::set_intersection(raw_arr, raw_arr + len, arr.begin(), arr.end(), res.begin());
+      res.resize(it - res.begin());
+      // TODO we need to get offsets.
+    }
+    else {
+      // Otherwise, we iterate the elements in the input vector.
+      for (int64_t i = 0; i < len; i++) {
+        if (map.contain(raw_arr[i])) {
+          res.push_back(raw_arr[i]);
+          offs.push_back(i);
+        }
+      }
+    }
+
+    return std::pair<std::vector<T>, std::vector<int64_t>>(res, offs);
+  }
+};
+
+std::shared_ptr<BufferData<dgl_id_t>> buf;
+
+DGL_REGISTER_GLOBAL("sampling.neighbor._CAPI_DGLCreateSampleBuffer")
+.set_body([] (DGLArgs args, DGLRetValue *rv) {
+    IdArray buf_arr = args[0];
+    int64_t len = buf_arr->shape[0];
+    const dgl_id_t *buf_data = static_cast<dgl_id_t*>(buf_arr->data);
+    buf = std::shared_ptr<BufferData<dgl_id_t>>(new BufferData<dgl_id_t>(buf_data, len));
+  });
+
+HeteroSubgraph SampleNeighborsWithBuffer(
+    const HeteroGraphPtr hg,
+    IdArray seed_nodes) {
+  CHECK_EQ(hg->NumEdgeTypes(), 1);
+  const dgl_id_t *seed_data = static_cast<dgl_id_t *>(seed_nodes->data);
+  int64_t num_seeds = seed_nodes->shape[0];
+
+  // Sample edges.
+  std::vector<dgl_id_t> src_ids, dst_ids, eids;
+  for (int64_t i = 0; i < num_seeds; i++) {
+    dgl_id_t seed = seed_data[i];
+    auto neigh_it = hg->PredVec(0, seed);
+    auto edge_it = hg->InEdgeVec(0, seed);
+    auto res = buf->lookup(neigh_it.begin(), neigh_it.end() - neigh_it.begin());
+    auto &sampled_ids = res.first;
+    auto &offs = res.second;
+    assert(offs.size() == sampled_ids.size());
+    for (size_t j = 0; j < sampled_ids.size(); j++) {
+      src_ids.push_back(seed);
+      dst_ids.push_back(sampled_ids[j]);
+      eids.push_back(edge_it.begin()[offs[j]]);
+    }
+  }
+
+  // Move data to IdArrays
+  int64_t num_sampled = src_ids.size();
+  IdArray src = IdArray::Empty({num_sampled},
+                               DLDataType{kDLInt, sizeof(dgl_id_t) * 8, 1}, DLContext{kDLCPU, 0});
+  memcpy(src->data, src_ids.data(), num_sampled * sizeof(dgl_id_t));
+  IdArray dst = IdArray::Empty({num_sampled},
+                               DLDataType{kDLInt, sizeof(dgl_id_t) * 8, 1}, DLContext{kDLCPU, 0});
+  memcpy(dst->data, dst_ids.data(), num_sampled * sizeof(dgl_id_t));
+  IdArray eid = IdArray::Empty({num_sampled},
+                               DLDataType{kDLInt, sizeof(dgl_id_t) * 8, 1}, DLContext{kDLCPU, 0});
+  memcpy(eid->data, eids.data(), num_sampled * sizeof(dgl_id_t));
+
+  // Construct the subgraph.
+  auto subg = UnitGraph::CreateFromCOO(hg->GetRelationGraph(0)->NumVertexTypes(),
+                                       hg->NumVertices(0),
+                                       hg->NumVertices(0),
+                                       src, dst);
+  HeteroSubgraph ret;
+  std::vector<HeteroGraphPtr> subgs(1);
+  subgs[0] = subg;
+  ret.graph = CreateHeteroGraph(hg->meta_graph(), subgs, hg->NumVerticesPerType());
+  ret.induced_vertices.resize(hg->NumVertexTypes());
+  std::vector<IdArray> induced_edges(1);
+  induced_edges[0] = eid;
+  ret.induced_edges = induced_edges;
+  return ret;
+}
+
+DGL_REGISTER_GLOBAL("sampling.neighbor._CAPI_DGLSampleWithBuffer")
+.set_body([] (DGLArgs args, DGLRetValue *rv) {
+    HeteroGraphRef hg = args[0];
+    IdArray seed_nodes = args[1];
+
+    std::shared_ptr<HeteroSubgraph> subg(new HeteroSubgraph);
+    *subg = sampling::SampleNeighborsWithBuffer(hg.sptr(), seed_nodes);
+
+    *rv = HeteroGraphRef(subg);
+  });
+
 }  // namespace sampling
 }  // namespace dgl
